48924088c2b91e60a51e12c9e4f9727b00cd15da
zjkjzj <wy163zhuj@163.com>
2020-09-07 21:48:28 +0800

    feat(optim): 更新为配置文件模式

--- tsn/optim/build.py
+++ tsn/optim/build.py
@@ -2,21 +2,27 @@
 
 """
 @date: 2020/8/21 下午7:55
-@file: build.py
+@file: trainer.py
 @author: zj
 @description: 
 """
 
+import torch.nn as nn
 import torch.optim as optim
+from torch.optim.optimizer import Optimizer
+
+from . import registry
+from .lr_schedulers.step_lr import build_step_lr
+from .lr_schedulers.multistep_lr import build_multistep_lr
+from .optimizers.sgd import build_sgd
+from .optimizers.adam import build_adam
 
 
 def build_optimizer(cfg, model):
-    return optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-5)
-    # return optim.Adam(model.parameters(), lr=3e-4, weight_decay=1e-5)
+    assert isinstance(model, nn.Module)
+    return registry.OPTIMIZERS[cfg.OPTIMIZER.NAME](cfg, model)
 
 
 def build_lr_scheduler(cfg, optimizer):
-    milestones = cfg.LR_SCHEDULER.MILESTONES
-    gamma = cfg.LR_SCHEDULER.GAMMA
-
-    return optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=gamma)
+    assert isinstance(optimizer, Optimizer)
+    return registry.LR_SCHEDULERS[cfg.LR_SCHEDULER.NAME](cfg, optimizer)
