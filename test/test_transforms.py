35a824cb82f648ec97f780d6d0ad13d8c6a81b61
zjkjzj <wy163zhuj@163.com>
2020-11-06 11:22:59 +0800

    test(transform): 更新opencv_transforms和torchvsion transforms测试时间比较

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -13,6 +13,44 @@ import time
 from tsn.data.transforms.build import build_transform
 from tsn.config import cfg
 
+"""
+opencv_transform vs. torchvision transform
+
+torchvhsion transform 测试
+$ python test/test_transforms2.py 
+torch.Size([3, 256, 256])
+process time: 0.015646696090698242
+torch.Size([3, 224, 224])
+process time: 0.042925119400024414
+$ python test/test_transforms2.py 
+torch.Size([3, 256, 256])
+process time: 0.020355701446533203
+torch.Size([3, 224, 224])
+process time: 0.021254539489746094
+$ python test/test_transforms2.py 
+torch.Size([3, 256, 256])
+process time: 0.015828847885131836
+torch.Size([3, 224, 224])
+process time: 0.03472018241882324
+
+opencv_transform 测试
+$ python test/test_transforms.py 
+torch.Size([3, 256, 256])
+process time: 0.011228799819946289
+torch.Size([3, 224, 224])
+process time: 0.02078700065612793
+$ python test/test_transforms.py 
+torch.Size([3, 256, 256])
+process time: 0.018286943435668945
+torch.Size([3, 224, 224])
+process time: 0.02521538734436035
+$ python test/test_transforms.py 
+torch.Size([3, 256, 256])
+process time: 0.00945901870727539
+torch.Size([3, 224, 224])
+process time: 0.02595067024230957
+"""
+
 
 def test_transforms():
     img = np.random.randn(224, 224, 3).astype(np.uint8)
